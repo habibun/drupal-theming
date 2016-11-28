7e734b7f98b038e9aa3ea3731c0a11a33fe9d391
Habibun Noby <habibun.noby@gmail.com>
2016-11-29 00:40:42 +0600

    introduction to views

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -753,7 +753,7 @@ if (file_exists(__DIR__ . '/settings.local.php')) {
   include __DIR__ . '/settings.local.php';
 }
 $databases['default']['default'] = array (
-  'database' => 'dp',
+  'database' => 'drupal',
   'username' => 'root',
   'password' => '',
   'prefix' => '',
